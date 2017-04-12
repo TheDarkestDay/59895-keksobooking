0225532dafbed983cca6ed60a3ec99378e3fe4a2
AlexanderBrenchev <alexzund3r@yahoo.com>
2017-04-12 23:32:57 +0700

    Laggy pin ddragging

--- js/map.js
+++ js/map.js
@@ -8,6 +8,15 @@
     }
   }
 
+  function enableDrag(evt) {
+    evt.preventDefault();
+    isDragging = true;
+  }
+
+  function disableDrag() {
+    isDragging = false;
+  }
+
   function closeOfferDetails() {
     offerCard.close();
     deactivateAllPins();
@@ -25,6 +34,18 @@
     });
   }
 
+  function moveLocationSelector(evt) {
+    if (!isDragging) {
+      return false;
+    }
+    var pinElem = evt.target;
+    if (pinElem.matches('img')) {
+      pinElem = evt.target.parentElement;
+    }
+    evt.target.style.top = (evt.pageY - mapContainer.offsetTop - locationSelector.offsetHeight / 2) + 'px';
+    evt.target.style.left = (evt.pageX - mapContainer.offsetLeft - locationSelector.offsetWidth / 2) + 'px';
+  }
+
   function openOfferDetailsOnClick(evt) {
     var pinElem = evt.target;
     if (pinElem.matches('img')) {
@@ -37,12 +58,15 @@
     offerCard.update(offersData[offerIdx].offer);
   }
 
-  var map = document.querySelector('.tokyo__pin-map');
+  var mapContainer = document.querySelector('.tokyo');
+  var map = mapContainer.querySelector('.tokyo__pin-map');
+  var locationSelector = map.querySelector('.pin__main');
   var closeDialogBtn = document.querySelector('.dialog__close');
   var offersFragment = document.createDocumentFragment();
 
   var ENTER = 13;
   var ESCAPE = 27;
+  var isDragging = false;
 
   window.offersData.forEach(function (offer, idx) {
     var nextOffer = pin.render(offer, idx);
@@ -60,5 +84,11 @@
 
   closeDialogBtn.addEventListener('click', closeOfferDetails);
   document.addEventListener('keydown', closeOfferDetailsFromKeyboard);
+
+  locationSelector.addEventListener('mousedown', enableDrag);
+  locationSelector.addEventListener('mouseup', disableDrag);
+  locationSelector.addEventListener('mouseleave', disableDrag);
+
+  locationSelector.addEventListener('mousemove', moveLocationSelector);
 
 })(window.utils, window.offerCard, window.offersData, window.pin);
